5bc644a6a610b7db0318e850f46dbb7fbc52c3c7
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-09-01 19:52:33 -0700

    Basic Username Validation

--- app/src/main/java/com/example/flightticket/CreateActivity.java
+++ app/src/main/java/com/example/flightticket/CreateActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateActivity extends AppCompatActivity {
 
     private EditText usernameText;
     private EditText passwordText;
     private Button createButton;
+
+    List<String> usernames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +28,34 @@ public class CreateActivity extends AppCompatActivity {
         passwordText = findViewById(R.id.passwordEdit);
         createButton = findViewById(R.id.createAccountButton);
 
+        // Temporary list for testing purposes. Will be replaced with UserDAO
+        usernames = new ArrayList<>();
+        usernames.add("Test");
+        usernames.add("Meow");
+
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // if validate(), add account & go to next activity
+                if(validate(usernameText.getText().toString(), usernames)){
+
+                }
 
             }
         });
     }
 
+    public boolean validate(String username, List<String> names){
+        // Search db for existing username
+        for (String name : names){
+            if (username.equals(name)){
+                Toast.makeText(this, "That username is already taken!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+        // TODO: Check for empty fields
 
+        return true;
+    }
 
 }
--- app/src/test/java/com/example/flightticket/ExampleUnitTest.java
+++ app/src/test/java/com/example/flightticket/ExampleUnitTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +16,16 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void validateUsername(){
+        CreateActivity activity = new CreateActivity();
+        List<String> usernames = new ArrayList<>();
+        usernames.add("Test");
+
+        assertTrue(activity.validate("Yest", usernames));
+        assertFalse(activity.validate("Test", usernames));
     }
 }
